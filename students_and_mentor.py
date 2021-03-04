ce1fb92ad0b439571cd9d098d99868ae8133dec7
zaur-tarverdizade <ztarverdizade@slb.com>
2021-03-04 23:29:11 +0400

    Добавление возможности сравнения студентов и учителей

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -1,12 +1,11 @@
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-
+        self.avg_grade = []
 
     def rate_mntr(self, mentor, course, grade):
         if isinstance(mentor, lecturer) and course in self.courses_in_progress and course in mentor.courses_attached:
@@ -19,10 +18,14 @@ class Student:
         else:
             print( 'Ошибка')
 
+    def avrg_rd(self):
+        for name_course, grade in self.grades.items():
+            average_grade = sum(grade) / len(grade)
+            self.avg_grade.append(name_course + " -> " + str(average_grade))
+
     def __str__(self):
         return "Имя:" + self.name + "\nФамилия:" + self.surname + "\nКурсы в процессе изучения: " + ",".join(self.courses_in_progress) + "\nЗавершенные курсы: " + ",".join(self.finished_courses) + "\nСредняя оценка за домашние задания: " + " , ".join(self.avg_grade)
 
-        
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -37,19 +40,23 @@ class lecturer(Mentor):
         self.grades = {}
         self.avg_grade = []
         self.grade = []
-        
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
+
+    def avrg_rd(self):
+        for name_course, grade in self.grades.items():
+            average_grade = sum(grade) / len(grade)
+            self.avg_grade.append(name_course + " -> " + str(average_grade))
+
+    def com(self, mentor):
+        if isinstance(mentor, lecturer):
+            if sum(mentor.grade) / len(mentor.grade) > sum(self.grade) / len(self.grade):
+                print("Учитель с большей оценкой: " + mentor.name + mentor.surname)
+            elif sum(mentor.grade) / len(mentor.grade) < sum(self.grade) / len(self.grade):
+                print("Учитель с большей оценкой: " + self.name + self.surname)
             else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+                return "Одинаковый"
 
     def __str__(self):
         return "Имя:" + self.name + "\nФамилия:" + self.surname + "\nСредняя оценка за лекции: " + " ".join(self.avg_grade)
-
 
 class reviwer(Mentor):
     def __init__(self, name, surname):
@@ -71,8 +78,29 @@ class reviwer(Mentor):
         else:
             return 'Ошибка'
 
+    def avg_ttl_grds(self, course):
+        if course in self.total_grades:
+            print("Средняя оценка за д/з по курсу "  + course + ": " + str(sum(self.total_grades.get(course))/len(self.total_grades.get(course)) ))
+
+    def avg_grade(self,student, course):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                average_grade = sum(student.grades[course])/len(student.grades[course])
+                print(average_grade)
+        else:
+            print("Ошибка")
+
+    def comp(self, student1, student2, name_course):
+        if isinstance(student1, Student) and isinstance(student2, Student) and name_course in self.courses_attached and name_course in student1.courses_in_progress and name_course in student2.courses_in_progress:
+            if sum(student1.grades[name_course]) / len(student1.grades[name_course]) > sum(student2.grades[name_course]) / len(student2.grades[name_course]):
+                print("Студент с большей оценкой по курсу " + name_course +  " : " + student1.name + ' ' + student1.surname)
+
+            elif sum(student1.grades[name_course]) / len(student1.grades[name_course]) < sum(student2.grades[name_course]) / len(student2.grades[name_course]):
+                print("Студент с большей оценкой по курсу " + name_course +  " : " + student2.name + ' ' + student2.surname)
+
     def __str__(self):
         return "Имя:" + self.name + "\nФамилия:" + self.surname
+
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
